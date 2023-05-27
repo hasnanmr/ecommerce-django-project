5030dca7364a4fe50985f591d9a1cf52a3d988be
unknown <hasnan.mhr@gmail.com>
2023-05-27 21:23:12 +0700

    change settings and .env

--- ecomm/ecomm/settings.py
+++ ecomm/ecomm/settings.py
@@ -99,23 +99,23 @@ WSGI_APPLICATION = 'ecomm.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'anton_db',
-        'USER': 'postgres',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': BASE_DIR / 'db.sqlite3',
+     }
+ }
+
+#DATABASES = {
+    #'default': {
+        #'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        #'NAME': 'anton_db',
+        #'USER': 'postgres',
+        #'PASSWORD': '',
+        #'HOST': 'localhost',
+        #'PORT': '5432',
+ #   }
+#}
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
@@ -196,18 +196,25 @@ if DEBUG is False:
     ALLOWED_HOSTS = ['www.domain-kita.com']
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'db_name',
-            'USER': 'db_user',
-            'PASSWORD': 'db_password',
-            'HOST': 'www.domain-kita.com',
-            'PORT': '5432',
-        }
-    }
+    #DATABASES = {
+        #'default': {
+            #'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            #'NAME': 'db_name',
+            #'USER': 'db_user',
+            #'PASSWORD': 'db_password',
+            #'HOST': 'www.domain-kita.com',
+            #'PORT': '5432',
+       # }
+    #}
 
     PAYPAL_CLIENT_ID = env('PAYPAL_LIVE_CLIENT_ID')
     PAYPAL_SECRET_ID = env('PAYPAL_LIVE_SECRET_ID')
     PAYPAL_TEST=False
     PAYPAL_RECEIVER_EMAIL = env('PAYPAL_RECEIVER_EMAIL')
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
